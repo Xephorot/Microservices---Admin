2159b582330ba12ea407003373a52fc8a1dffd5c
Sergio Escalante <escalantesergio821@gmail.com>
2024-11-04 18:49:39 -0400

    Cambiado a longtext

--- src/product/product.entity.ts
+++ src/product/product.entity.ts
@@ -1,16 +1,16 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column()
-    title: string;
+  @Column()
+  title: string;
 
-    @Column()
-    image: string;
+  @Column({ type: 'longtext' })
+  image: string;
 
-    @Column({ default: 0})
-    likes:number;
+  @Column({ default: 0 })
+  likes: number;
 }
